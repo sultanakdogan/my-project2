88c398f4b11eda76cc474fdb13f182d89b9facaa
Sultan Akdogan <sultanakdogan026@gmail.com>
2022-12-28 00:26:30 +0000

    aritmetic scripts examples

---
+++ aritmetic.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# echo "how are you doing!"
+# read -p "enter your first number: " a
+# read -p "enter your second number : " b
+
+# echo `expr $a + $b`
+# echo `expr $a - $b`
+# echo `expr $a \* $b`
+# echo `expr $a / $b`
+# let "sum = 3 + 5"
+# echo $sum
+# read -p "Input first number: " first_number
+# read -p "Input second number: " second_number
+
+# let "sum = $first_number + $second_number"
+# let "sub = $first_number - $second_number"
+# let "mul = $first_number * $second_number"
+# let "div = $first_number / $second_number"
+# echo "SUM=$sum"
+# echo "SUB=$sub"
+# echo "MUL=$mul"
+# echo "DIV=$div"
+# x=10
+# let y=x
+# echo "$x"
+# echo "$y"
+# sum=$((3+5))
+# echo $sum
+# read -p "Input first number: " first_number
+# read -p "Input second number: " second_number
+
+# sum=$(($first_number + $second_number))
+# sub=$(($first_number - $second_number))
+# mul=$(($first_number * $second_number))
+# div=$(($first_number / $second_number))
+
+# echo "SUM=$sum"
+# echo "SUB=$sub"
+# echo "MUL=$mul"
+# echo "DIV=$div"
+
+# (( first_number++ ))
+# (( second_number-- ))
+
+# echo "The increment of first number is $first_number"
+# echo "The decrement of second number is $second_number"
